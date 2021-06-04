aed04c0c2d8cb1f9a06cd3fdda11a652373f3576
Johan Walles <johan.walles@gmail.com>
2021-06-04 22:44:46 +0200

    Get sources using git instead
    This way the build scripts can do version information.

--- Formula/moar.rb
+++ Formula/moar.rb
@@ -1,8 +1,9 @@
 class Moar < Formula
   desc "A pager that is designed to be nice to use"
   homepage "https://github.com/walles/moar"
-  url "https://github.com/walles/moar/archive/refs/tags/v1.8.1.tar.gz"
-  sha256 "f4edbca2cce46e64d033536958d813ed70e7668ab52316cb87e42cccdb00eaaf"
+  url "https://github.com/walles/moar.git",
+    using:    :git,
+    tag:      "v1.8.1"
   license "BSD-2-Clause"
 
   depends_on "go" => :build
