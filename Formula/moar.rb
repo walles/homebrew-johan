583f18b00d962b0e6f44c3bb8fef484997bd3ded
Johan Walles <johan.walles@gmail.com>
2021-06-05 08:52:58 +0200

    Make brew audit and style happy

--- Formula/moar.rb
+++ Formula/moar.rb
@@ -1,9 +1,10 @@
 class Moar < Formula
-  desc "A pager that is designed to be nice to use"
+  desc "Pager that is designed to be nice to use"
   homepage "https://github.com/walles/moar"
   url "https://github.com/walles/moar.git",
     using:    :git,
-    tag:      "v1.8.1"
+    tag:      "v1.8.1",
+    revision: "fec9ad814970ee1a271b19326214647460b3200c"
   license "BSD-2-Clause"
 
   depends_on "go" => :build
