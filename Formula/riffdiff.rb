fac58d2eedfeb4f45f8d6aa9e7a0ecc8f2458b4f
Johan Walles <johan.walles@gmail.com>
2021-06-06 17:01:28 +0200

    Add formula for riff, the refining diff
    Source: https://github.com/walles/riff

---
+++ Formula/riffdiff.rb
@@ -0,0 +1,20 @@
+class Riffdiff < Formula
+  desc "Diff filter highlighting which line parts have changed"
+  homepage "https://github.com/walles/riff"
+  url "https://github.com/walles/riff.git",
+    using:    :git,
+    tag:      "2.9",
+    revision: "1654367e4716c0af75973b7e0286b119a72b455e"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # Test with two identical files, should output nothing and succeed
+    assert_equal "", shell_output("riff /etc/passwd /etc/passwd").strip
+  end
+end
