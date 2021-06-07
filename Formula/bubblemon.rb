86845feb1082119976ae185079ffa691fba74fda
Johan Walles <johan.walles@gmail.com>
2021-06-07 03:14:48 +0200

    Add initial Bubblemon formula

---
+++ Formula/bubblemon.rb
@@ -0,0 +1,43 @@
+class Bubblemon < Formula
+  # Can be bumped to latest master at any time, get your SHAs here:
+  # https://github.com/walles/bubblemon/commits/master
+  #
+  # NOTE: Update VERSION (below) if you do this
+  REVISION = "8e5cc56adb0fb1a5f9d5a0e13113d4351e6b6ef3".freeze
+
+  # YYYY.MM.DD of when REVISION ^ was committed
+  #
+  # NOTE: Update together with REVISION (above)
+  VERSION = "2021.03.03".freeze
+
+  desc "System load visualization for human beings"
+  homepage "https://walles.github.io/bubblemon"
+  url "https://github.com/walles/bubblemon.git",
+    using:    :git,
+    revision: REVISION
+  version VERSION
+  license "GPL-2.0-or-later"
+
+  depends_on xcode: :build
+  depends_on :macos
+
+  def pour_bottle?
+    # Must be built locally due to macOS security measures
+    false
+  end
+
+  def install
+    xcodebuild "build",
+      "-project", "osx/bubblemon.xcodeproj",
+      "-target", "Bubblemon",
+      "-target", "Bubblemon TouchBar",
+      "-target", "Bubblemon Menu Bar"
+    prefix.install "build/Release/Bubblemon.app"
+    prefix.install "build/Release/Bubblemon TouchBar.app"
+    prefix.install "build/Release/Bubblemon Menu Bar.app"
+
+    # FIXME: open "/Applications/Bubblemon.app" --args --reinstall
+    # FIXME: open "/Applications/Bubblemon Menu Bar.app"
+    # FIXME: open "/Applications/Bubblemon TouchBar.app" --args --reinstall
+  end
+end
