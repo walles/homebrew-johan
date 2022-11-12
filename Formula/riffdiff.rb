1703788299ebeab33ecad5e0ed16a5d6c3661a7d
Johan Walles <johan.walles@gmail.com>
2022-11-12 16:48:44 +0100

    Update riffdiff.rb (#22)

--- Formula/riffdiff.rb
+++ Formula/riffdiff.rb
@@ -3,8 +3,8 @@ class Riffdiff < Formula
   homepage "https://github.com/walles/riff"
   url "https://github.com/walles/riff.git",
     using:    :git,
-    tag:      "2.21.0",
-    revision: "62da46c7b300321119d399bdc69bfb2d56d5da57"
+    tag:      "2.22.0",
+    revision: "4ea55bf641a41a58759c63c6ddd2138da57fb00e"
   license "MIT"
 
   depends_on "rust" => :build
