37e5c25f388a8bca563ffca50a4b6e2bf14d01db
Johan Walles <johan.walles@gmail.com>
2023-06-24 10:14:17 +0200

    LoadViz 0.1.0 (#32)

--- Casks/loadviz.rb
+++ Casks/loadviz.rb
@@ -1,8 +1,8 @@
 cask "loadviz" do
-  version "0.0.1"
-  sha256 "8a05075c2df4e0847bfc86fe876456d7d66eeeecc80a7d361a5d078562f31e38"
+  version "0.1.0"
+  sha256 "859d4f673a4ca7ab5cae51be59a1291f757c5652f2c93e062ff7d0ed1988bec4"
 
-  url "https://github.com/walles/loadviz/releases/download/macos-#{version}/LoadViz-#{version}.tar.bz2"
+  url "https://github.com/walles/loadviz/releases/download/macos-#{version}/LoadViz-macos-#{version}.tar.bz2"
   name "LoadViz"
   desc "System Load Visualization in the Menu Bar"
   homepage "https://github.com/walles/loadviz/"
