32ad4f921e4dfb424cab2b4f98660a6dbd2f8bc9
Johan Walles <johan.walles@gmail.com>
2023-08-17 23:01:40 +0200

    Update loadviz.rb (#48)

--- Casks/loadviz.rb
+++ Casks/loadviz.rb
@@ -1,6 +1,6 @@
 cask "loadviz" do
-  version "1.2.1"
-  sha256 "55818f6cb9c0fdab50c58622b8f69ce2f6d4d15bb1128d01cbf7780ec123817d"
+  version "1.2.2"
+  sha256 "15795fcf3e14857af7b361554a64009de581e152572f119fd967b518550edff4"
 
   url "https://github.com/walles/loadviz/releases/download/macos-#{version}/LoadViz-macos-#{version}.tar.bz2"
   name "LoadViz"
