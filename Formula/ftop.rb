7356b744d48ace0d61cb33fb1383c2f4b0522601
Johan Walles <johan.walles@gmail.com>
2026-02-14 07:17:26 +0100

    Add an ftop formula
    Untested.

---
+++ Formula/ftop.rb
@@ -0,0 +1,21 @@
+class Ftop < Formula
+  desc "top for Human Beings"
+  homepage "https://github.com/walles/ftop"
+  url "https://github.com/walles/ftop/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "c8cda9350c427ff569ee7768db7cb6cdb203e1bd046759d071383dfae26b6d94"
+  license "MIT"
+  head "https://github.com/walles/ftop.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    # Linker flags source:
+    # https://github.com/walles/ftop/blob/22109ac57068973b83c9a6cbb8445147efd74bb6/build.sh#L33
+    ldflags = "-s -w -X main.versionString=v#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/ftop"
+  end
+
+  test do
+    assert_match "v#{version}", shell_output("#{bin}/ftop --version").strip
+  end
+end
