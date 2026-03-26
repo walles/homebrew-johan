8688e80eccfc9f80c58ded4bd04130a151af5c39
Johan Walles <johan.walles@gmail.com>
2026-03-26 07:56:38 +0100

    ftop: update to v0.0.15

--- Formula/ftop.rb
+++ Formula/ftop.rb
@@ -1,8 +1,8 @@
 class Ftop < Formula
   desc "Top for Human Beings"
   homepage "https://github.com/walles/ftop"
-  url "https://github.com/walles/ftop/archive/refs/tags/v0.0.14.tar.gz"
-  sha256 "6d595d40dcce50aee284bbc941d4339b0c540e4a940624029f51cb72b94616f1"
+  url "https://github.com/walles/ftop/archive/refs/tags/v0.0.15.tar.gz"
+  sha256 "fb4cd7a2fb889c9bf12800994f387f97ea540cf861e5cdd1282d27d9ab8dbf4d"
   license "MIT"
   head "https://github.com/walles/ftop.git", branch: "main"
 
